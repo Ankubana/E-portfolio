9e9c7d6436ca4c93adc06e3ef00628147a67507b
Alphonse <kubalpha19@gmail.com>
2025-07-29 13:10:23 -0700

    updated filies

--- src/App.js
+++ src/App.js
@@ -7,27 +7,26 @@ import Modal from './components/Modal';
 import Footer from './components/Footer';
 import BackgroundShapes from './components/BackgroundShapes';
 import Resume from './components/Resume';
-import { BrowserRouter as Router,Routes,Route,useLocation,} from "react-router-dom";
-// Move logic into a sub-component INSIDE the Router
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
 function AppContent() {
   const location = useLocation();
   const isResumePage = location.pathname === "/Resume";
 
   return (
-    <> 
-       {!isResumePage && <BackgroundShapes />}
-       
+    <>
+      {!isResumePage && <BackgroundShapes />}
       {!isResumePage && <Navbar />}
       {!isResumePage && <Hero />}
       {!isResumePage && <Modal />}
       {!isResumePage && <Projects />}
-      
       <Routes>
         <Route path="/Resume" element={<Resume />} />
       </Routes>
     </>
   );
 }
+
 function App() {
   return (
     <Router>
@@ -35,5 +34,5 @@ function App() {
     </Router>
   );
 }
-m
+
 export default App;
